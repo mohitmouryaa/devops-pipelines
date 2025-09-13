31f66d34fca61c7889a3cd62dc50178da507140c
Mohit Mourya <mohitmouryaa143@gmail.com>
2025-09-13 19:17:28 +0530

    feat: add CI/CD pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("HELLO FROM DEVOPS");
+console.log("Hey, I'm learning CI/CD using Github Actions");
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("Starting tests...");
+setTimeout(() => {
+  console.log("Waiting 3 seconds...");
+  console.log("Tests complete!.")
+}, 3000);
